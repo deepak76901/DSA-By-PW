869b357b82766c0d8e37af4511227521e264f1ec
deepak76901 <deepakb76901@gmail.com>
2023-11-07 15:02:25 +0530

    pushing

---
+++ #Notes/Time ,Space and Arrays/Codes/Try.java
@@ -0,0 +1,17 @@
+public class Try{
+    public static void main(String[] args) {
+        
+        int [] arr = {1,2,3,4};
+        
+        for(int i=0 ; i<arr.length ; i++){
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+        
+        System.out.println();
+        for(int a : arr){
+            System.out.println(a);
+        }
+    }
+
+}
